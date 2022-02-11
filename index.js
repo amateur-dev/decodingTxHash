37536ac2d2f393f901bb62a40c7e9889d905c580
amateur_dev <49892677+amateur-dev@users.noreply.github.com>
2022-02-11 13:30:05 +0000

    Clean up

--- index.js
+++ index.js
@@ -1,2 +1,56 @@
-const getTxDetails = require("./getTxDetails");
-getTxDetails("0x4738de5f8c29e1ea31fb2ec0baf9b008afc99d1da6a14c147f2b019d5f70c118");
+const ethers = require('ethers');
+const provider = ethers.getDefaultProvider("rinkeby");
+;
+const newWallet = ethers.Wallet.createRandom();
+
+console.log(newWallet);
+
+
+
+// const iface = new ethers.utils.Interface([
+//   // Constructor
+//   "constructor(string symbol, string name)",
+
+//   // State mutating method
+//   "function transferFrom(address from, address to, uint amount)",
+
+//   // State mutating method, which is payable
+//   "function mint(uint amount) payable",
+
+//   // Constant method (i.e. "view" or "pure")
+//   "function balanceOf(address owner) view returns (uint)",
+
+//   // An Event
+//   "event Transfer(address indexed from, address indexed to, uint256 amount)",
+
+//   // A Custom Solidity Error
+//   "error AccountLocked(address owner, uint256 balance)",
+
+//   // Examples with structured types
+//   "function addUser(tuple(string name, address addr) user) returns (uint id)",
+//   "function addUsers(tuple(string name, address addr)[] user) returns (uint[] id)",
+//   "function getUser(uint id) view returns (tuple(string name, address addr) user)"
+// ]);
+
+
+
+// const ethers = require('ethers');
+// const utils = require('ethers/utils'); 
+
+
+
+// let emailHex = utils.hashMessage("dipeshsukhani@gmail.com");
+
+// let dobHex = utils.hashMessage("28091985");  // required to be a string 
+// let passwordHex = utils.hashMessage("thisIsATestPasswordWhichWillBeHashedUsingASaltInTheServer"); // will ensure that the client will be one of the controlling party in generating the account
+// let saltMessageHex = utils.hashMessage("SaltMessageInString"); // will ensure that 1xchange will be one of the controlling party in generating the account
+
+// let concatedMessage = ((emailHex.concat(dobHex)).concat(passwordHex)).concat(saltMessageHex);
+// let hexOfConcatedMessage = utils.hashMessage(concatedMessage);
+// console.log(hexOfConcatedMessage);
+
+// let wallet = new ethers.Wallet(hexOfConcatedMessage);
+// console.log(wallet);
+
+// let randomWallet = ethers.Wallet.createRandom();
+// console.log(randomWallet);
