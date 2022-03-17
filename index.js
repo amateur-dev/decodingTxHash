b5b1145efe61d206ef2ce5cf3101134430c81d4a
amateur_dev <49892677+amateur-dev@users.noreply.github.com>
2022-03-17 08:00:36 +0000

    some code

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const ethers = require('ethers');
-const provider = ethers.getDefaultProvider("rinkeby");
-;
-const newWallet = ethers.Wallet.createRandom();
+// const provider = ethers.getDefaultProvider("rinkeby");
+
+const newWallet = ethers.Wallet.createRandom({} );
 
 console.log(newWallet);
-
-
